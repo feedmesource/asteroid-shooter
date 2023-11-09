66c9515b48192da09aea05523774ea96d07b922c
aimartuul <aituul@taltech.ee>
2023-11-09 03:34:46 +0200

    Refactor hard-coded values and some other changes
    Change magic numbers into editable variables.
    Edit some variable names.
    Add "game over" text before closing game.
    Change speed to 60FPS.
    Implement closing game window exit.
    Add .gitignore row.

--- main.py
+++ main.py
@@ -2,66 +2,70 @@ import math
 import pygame
 import random
 
-
 pygame.init()
-myfont = pygame.font.SysFont("monospace", 20, bold=True)
+my_font = pygame.font.SysFont("monospace", 20, bold=True)
 
-
-screen = pygame.display.set_mode((800, 800))
+display_width = 1000
+display_height = 800
+screen = pygame.display.set_mode((display_width, display_height))
 
 clock = pygame.time.Clock()
 
 run = True
 
 class Ship():
-    def __init__(self, startx, starty) -> None:
-        self.x = startx
-        self.y = starty
+    def __init__(self, start_x, start_y) -> None:
+        self.x = start_x
+        self.y = start_y
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
         if self.x < 0:
             self.x = 0
-        elif self.x > 800:
-            self.x = 800
+        elif self.x > display_width:
+            self.x = display_width
         if self.y < 0:
             self.y = 0
-        elif self.y > 800:
-            self.y = 800
+        elif self.y > display_height:
+            self.y = display_height
 
 class Bullet():
-    def __init__(self, startx, starty) -> None:
-        self.x = startx
-        self.y = starty
+    def __init__(self, start_x, start_y) -> None:
+        self.x = start_x
+        self.y = start_y
     def move(self):
-        self.y -= 20
+        self.y -= 10
 
 class Asteroid():
-    def __init__(self, startx, starty, speed, size) -> None:
-        self.x = startx
-        self.y = starty
+    biggest_size = 55
+    
+    def __init__(self, start_x, start_y, speed, size) -> None:
+        self.x = start_x
+        self.y = start_y
         self.speed = speed
         self.size = size
     def move(self):
         self.y += self.speed
-        if self.y > 840:
-            self.y = -40
+        if self.y > display_height + self.biggest_size:
+            self.y = -self.biggest_size
 
-ship = Ship (400, 400)
+ship = Ship (display_width / 2, display_height / 2)
 bullets = []
 asteroids = []
 score = 0
 
 def generate_random_asteroid():
-    rand_startx = random.randint(0, 800)
-    rand_starty = random.randint(0, 200)
-    rand_speed = random.randint(1, 4)
-    rand_size = random.randint(8, 40)
+    rand_start_x = random.randint(0, display_width)
+    rand_start_y = random.randint(-2 * Asteroid.biggest_size, display_height / 4)
+    rand_speed = random.randint(1, 2)
+    rand_size = random.randint(7, Asteroid.biggest_size)
 
-    asteroid = Asteroid(rand_startx, rand_starty, rand_speed, rand_size)
+    asteroid = Asteroid(rand_start_x, rand_start_y, rand_speed, rand_size)
     return asteroid
 
-for i in range(30):
+count_of_asteroids = 25
+
+for i in range(count_of_asteroids):
     asteroid = generate_random_asteroid()
     asteroids.append(asteroid)
 
@@ -69,16 +73,19 @@ for i in range(30):
 def distance (ax, ay, bx, by) -> float:
     return math.sqrt((ax-bx)**2 + (ay - by)**2)
 
-while run:
-        
-    screen.fill("BLUE")
+exit = False
+
+while run:    
+    background_color = pygame.Color(66,37,4,204)
+    screen.fill(background_color)
+    
     events = pygame.event.get()
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_UP]: ship.move(0, -10)
-    if keys[pygame.K_DOWN]: ship.move(0, 10)
-    if keys[pygame.K_LEFT]: ship.move(-10, 0)
-    if keys[pygame.K_RIGHT]: ship.move(10, 0)
+    if keys[pygame.K_UP]: ship.move(0, -5)
+    if keys[pygame.K_DOWN]: ship.move(0, 5)
+    if keys[pygame.K_LEFT]: ship.move(-5, 0)
+    if keys[pygame.K_RIGHT]: ship.move(5, 0)
         
     pygame.draw.polygon(screen, "#FFFFFF", [[ship.x, ship.y], [ship.x - 10, ship.y + 30],  [ship.x + 10, ship.y + 30]], 2)
 
@@ -94,8 +101,9 @@ while run:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             bullet = Bullet(ship.x, ship.y)
             bullets.append(bullet)
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE or event.type == pygame.QUIT:
             run = False
+            exit = True
         
     
     for bullet in bullets:
@@ -115,10 +123,25 @@ while run:
                 break
 
     # render text
-    label = myfont.render(f"Score: {score}", 1, (255,0,0))
+    label = my_font.render(f"Score: {score}", 1, (200,0,150), background_color.normalize())
     screen.blit(label, (0, 0))
 
     pygame.display.update()
 
-    clock.tick(30)
+    clock.tick(60)
+
+
+if not exit:
+    game_over_font = pygame.font.SysFont("monospace", 80, True)
+    game_over_text = game_over_font.render("You lost!", 1, "RED")
+    text_rect = game_over_text.get_rect(center=(display_width / 2, display_height / 2))
+    screen.blit(game_over_text, text_rect)
+    pygame.display.update()
+
+while(not exit):
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN and (event.key == pygame.K_ESCAPE or event.key == pygame.K_SPACE) or event.type == pygame.QUIT:
+            run = False
+            exit = True
+
 pygame.quit()
